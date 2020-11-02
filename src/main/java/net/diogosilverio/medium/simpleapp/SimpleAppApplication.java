63de5a3d1c8eed2ef6714dc9b34cf82708661c7d
Diogo Silvério <dblsilverio+github@gmail.com>
2020-11-02 16:37:43 -0300

    Initial commit
    Starting with Jib

---
+++ src/main/java/net/diogosilverio/medium/simpleapp/SimpleAppApplication.java
@@ -0,0 +1,21 @@
+package net.diogosilverio.medium.simpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SimpleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAppApplication.class, args);
+    }
+
+    @GetMapping
+    public Integer theAnswer(){
+        return 42;
+    }
+
+}
